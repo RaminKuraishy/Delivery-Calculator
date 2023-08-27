c9b9b097acff656214c3376fab94ac286f6023a5
Ramin Kuraishy <ramin.kuraishy@gmail.com>
2023-08-27 15:34:14 +0300

    create slider range, buttons, and a little fix design part

--- delivery-calculator/src/component/Form/Form.js
+++ delivery-calculator/src/component/Form/Form.js
@@ -1,5 +1,44 @@
 import "./Form.scss";
+import Select from "react-select";
 function Form() {
+  const options = [
+    {
+      value: "France",
+      label: "Франція",
+    },
+    {
+      value: "Poland",
+      label: "Польща",
+    },
+    {
+      value: "Germany",
+      label: "Німеччина",
+    },
+  ];
+  const selectStyles = {
+    control: styles => ({
+      ...styles,
+      backgroundColor: "#f6f6f9",
+      border: "0",
+      padding: "3px 0 0 7px",
+      boxShadow: "none",
+      outline: "none",
+    }),
+    option: styles => {
+      return {
+        ...styles,
+        " &:hover": { color: "#da291c" },
+        color: "#000",
+        backgroundColor: "#F6F6F9",
+        border: "none",
+        outline: "none",
+      };
+    },
+    dropdownIndicator: base => ({
+      ...base,
+      "&:hover": { color: "#da291c" },
+    }),
+  };
   const rangeSlide = value => {
     const weight = document.querySelector(".weight-input");
     const range = document.querySelector(".input-range");
@@ -8,25 +47,39 @@ function Form() {
     });
     range.addEventListener("input", e => {
       updateWeight(e.target.value);
+      // console.log(e.target.value);
     });
 
     const updateWeight = currWeight => {
       currWeight = currWeight / 10;
       weight.value = currWeight;
-      // range.value = weight.value
     };
   };
+  const getPriceBtn = event => {
+    if (event.target.tagName === "BUTTON") {
+      const buttons = document.querySelectorAll(".price-presets-btn");
+      buttons.forEach(i => {
+        buttons.forEach(btn => {
+          btn.classList.remove("active-color");
+        });
+        event.target.classList.add("active-color");
+      });
+    }
+  };
+
   return (
     <form action="#" className="delivery-form">
       <div className="input-wrapper">
         <h2 className="select-title">Країна відправки</h2>
-        <select className="form-country" name="country" id="">
-          <option value="France">Франція</option>
-          <option value="Poland" defaultValue>
-            Польща
-          </option>
-          <option value="Germany">Німеччина</option>
-        </select>
+        <Select
+          options={options}
+          defaultValue={{
+            value: "Poland",
+            label: "Польща",
+          }}
+          styles={selectStyles}
+          onFocus={false}
+        />
       </div>
       <div className="input-wrapper">
         <h2 className="input-title">
@@ -51,6 +104,7 @@ function Form() {
           ></input>
         </div>
       </div>
+
       <div className="total-price">
         <h2 className="total-price-title">
           Приблизна вартість доставки (гривня)
@@ -63,7 +117,7 @@ function Form() {
           товарів разом з митом).
         </p>
       </div>
-      <div className="price-presets">
+      <div onClick={getPriceBtn} className="price-presets">
         <button className="price-presets-btn">Кросівки</button>
         <button className="price-presets-btn">Футболка</button>
         <button className="price-presets-btn">Сукня</button>
